151ec4054cf5ba4715bdb8b19b320f234a14659f
Karcheek666 <56692719+Karcheek666@users.noreply.github.com>
2020-10-05 11:56:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+// console.log('Hello');
+let name = 'Kartuk'; //String literal
+let age = 12; //num literal
+let isApproved = false; //boolean
+name = 'Voym';
+
+//Object
+
+let person = {
+    name: name
+};
+// person.name = 'Kartuk'
+person['name'] = 'Kartek'
+
+//Arrays
+let colors = ['blue','cyan'];
+colors[1] = 'blue';
+//console.log(colors.);
+
+//Functions
+
+// function greet(name,age,status){
+//     console.log('HELLO ' + name + age + status);
+// }
+
+// greet('Neji ',14,' dead');
+
+let constant = '+91' ;
+function phone_num(num){
+    return num;
+}
+
+let num = constant + phone_num(4738291059);
+console.log(num);
